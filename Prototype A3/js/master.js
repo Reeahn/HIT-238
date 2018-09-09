9f57d72012aa1d92282f4543107f745bb0fd49cb
Reeahn <ryan.toeta@gmail.com>
2018-09-09 18:36:23 +0930

    testing js on mobile

--- Prototype A3/js/master.js
+++ Prototype A3/js/master.js
@@ -6,6 +6,8 @@ function showDisplay(element) {
   element[0].style.display = "grid";
 };
 
+/* hides and shows menus when the main menu buttons are clicked */
+
 document.querySelector('#allergiesBtn')
   .addEventListener('click', function(evt) {
     hideDisplay(document.getElementsByClassName('menuButtons'));
@@ -31,3 +33,30 @@ document.querySelector('#backBtn')
   hideDisplay(document.getElementsByClassName('searchMenu'));
   showDisplay(document.getElementsByClassName('menuButtons'));
 });
+
+
+/* code from https://www.w3schools.com/howto/howto_js_accordion.asp
+  for the expandable menu in the allergies and dietry menus */
+
+var expMenu = document.getElementsByClassName("expandableList");
+var i;
+
+for (i = 0; i < expMenu.length; i++) {
+    expMenu[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+};
+/* code so that when all Nuts is selected, all the unselected nuts get checked
+and when all nuts is deselected, all the selected nuts get unchecked */
+
+console.log(document.getElementsByClassName("expandableList"))
