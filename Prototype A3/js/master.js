8a1a7bd315eb0875985f621c643dfcabfc8feef1
Reeahn <ryan.toeta@gmail.com>
2018-10-02 15:05:07 +0930

    update js var names and html class names
    made js for the checkbox all more expandable by putting it into its own function.
    Applied it to the vegetables checklist

--- Prototype A3/js/master.js
+++ Prototype A3/js/master.js
@@ -1,3 +1,7 @@
+var checkboxAll = document.getElementsByClassName("All");
+var checkboxSub1 = document.getElementsByClassName("Sub1");
+var checkboxSub2 = document.getElementsByClassName("Sub2");
+
 function hideDisplay(element) {
   Array.from(element).forEach((elem) => {
     elem.classList.add('hidden');
@@ -16,12 +20,14 @@ function showDisplay(element) {
 
 document.querySelector('#allergiesBtn')
   .addEventListener('click', function(evt) {
+    selector(checkboxSub1, checkboxAll[0])
     hideDisplay(document.getElementsByClassName('menuButtons'));
     showDisplay(document.getElementsByClassName('allergiesMenu'));
   });
 
 document.querySelector('#dietryBtn')
   .addEventListener('click', function(evt) {
+    selector(checkboxSub2, checkboxAll[1])
     hideDisplay(document.getElementsByClassName('menuButtons'));
     showDisplay(document.getElementsByClassName('dietryMenu'));
   });
@@ -65,38 +71,36 @@ for (i = 0; i < expMenu.length; i++) {
 /* code so that when all Nuts is selected, all the unselected nuts get checked
 and when all nuts is deselected, all the selected nuts get unchecked */
 
-var checkbox = document.querySelector("#allNuts");
-var nuts = document.getElementsByClassName("nut");
-
-checkbox.addEventListener('change', function() {
-    if(checkbox.checked) {
-        // Checkbox is checked..
-        for (i = 0; i < nuts.length; i++) {
-          nuts[i].checked = true;
-          deselectorForNuts(nuts[i],checkbox);
-        };
-    } else {
-        // Checkbox is not checked..
-        for (i = 0; i < nuts.length; i++) {
-          nuts[i].checked = false;
-          deselectorRemover(nuts,checkbox);
-        };
-    };
-});
-
+function selector(checkboxSub,checkboxAll) {
+  checkboxAll.addEventListener('change', function() {
+      if(checkboxAll.checked) {
+          // Checkbox is checked..
+          for (i = 0; i < checkboxSub.length; i++) {
+            checkboxSub[i].checked = true;
+            deselectorForSub(checkboxSub[i],checkboxAll);
+          };
+      } else {
+          // Checkbox is not checked..
+          for (i = 0; i < checkboxSub.length; i++) {
+            checkboxSub[i].checked = false;
+            deselectorRemover(checkboxSub[i],checkboxAll);
+          };
+      };
+  });
+};
 // When the nuts are deslected after allnuts is selected all nuts should be deslected
-function deselectorForNuts(nut,checkbox) {
-  nut.addEventListener('change', function() {
-    if(nut.checked === false) {
-      checkbox.checked = false;
+function deselectorForSub(sub,checkboxAll) {
+  sub.addEventListener('change', function() {
+    if(sub.checked === false) {
+      checkboxAll.checked = false;
     };
   });
 };
 // To minimise strain on the machine, the event listener will be removed when allNuts is unchecked
-function deselectorRemover(nuts,checkbox) {
-  nuts[i].removeEventListener('change', function() {
-    if(nut.checked === false) {
-      checkbox.checked = false;
+function deselectorRemover(sub,checkboxAll) {
+  sub.removeEventListener('change', function() {
+    if(sub.checked === false) {
+      checkboxAll.checked = false;
     };
   });
 };
