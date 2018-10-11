a880c2359d4c1418f79db5da69c71834260cc68c
Reeahn <36976247+Reeahn@users.noreply.github.com>
2018-10-11 19:03:12 +0930

    test activate code

--- Prototype A3/html/sw.js
+++ Prototype A3/html/sw.js
@@ -7,7 +7,7 @@ if ('serviceWorker' in navigator) {
 };
 
 var CACHE_TITLE = 'my-site-cache';
-var CACHE_VERSION = 'v2';
+var CACHE_VERSION = 'v1';
 var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION;
 var urlsToCache = [
   './index.html',
