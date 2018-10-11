235164dbe472470bcf9b20da6b0fa9952d40b954
Reeahn <36976247+Reeahn@users.noreply.github.com>
2018-10-11 17:35:54 +0930

    trying to fix error with service worker

--- Prototype A3/html/sw.js
+++ Prototype A3/html/sw.js
@@ -1,10 +1,14 @@
 if ('serviceWorker' in navigator) {
+  console.log("its here")
   window.addEventListener('load', function() {
+    console.log("loaded")
     navigator.serviceWorker.register('./sw.js').then(function(registration) {
       // Registration was successful
+      console.log("successful")
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       // registration failed :(
+      console.log("failed")
       console.log('ServiceWorker registration failed: ', err);
     });
   });
@@ -14,12 +18,13 @@ var CACHE_TITLE = 'my-site-cache';
 var CACHE_VERSION = 'v1';
 var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION;
 var urlsToCache = [
-  '../html/inex.html',
+  '../html/',
   '../css/master.css',
   '../js/master.js',
 ];
 
 self.addEventListener('install', function(event) {
+  console.log("install")
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
