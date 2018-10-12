aa12e5c60c2ad0ee5ff25dabd7fac7981c34ff3b
Reeahn <36976247+Reeahn@users.noreply.github.com>
2018-10-12 12:29:33 +0930

    Update Prototype A3/js/recipesAndSearch.js

--- Prototype A3/js/recipesAndSearch.js
+++ Prototype A3/js/recipesAndSearch.js
@@ -9,5 +9,4 @@ fetch('../JSON/recipes.json')
     for (x = 0; x < recipes["method"].length; x++) {
       document.getElementById('results').innerHTML += (x+1) + " " + recipes["method"][x] + "<br>" + "<br>";
     };
-
   });
