e74f9ee03d3d2e836c76ba54ceedde013bc2f31c
Reeahn <36976247+Reeahn@users.noreply.github.com>
2018-10-12 12:24:40 +0930

    touching up css

--- Prototype A3/js/recipesAndSearch.js
+++ Prototype A3/js/recipesAndSearch.js
@@ -5,7 +5,7 @@ fetch('../JSON/recipes.json')
   .then(function(myJson) {
     var recipes = myJson;
     console.log(Object.keys(recipes));
-    document.getElementById('results').innerHTML += recipes["name"] + "<br>";
+    document.getElementById('results').innerHTML += recipes["name"] + "<br>"  ;
     for (x = 0; x < recipes["method"].length; x++) {
       document.getElementById('results').innerHTML += (x+1) + " " + recipes["method"][x] + "<br>";
     };
