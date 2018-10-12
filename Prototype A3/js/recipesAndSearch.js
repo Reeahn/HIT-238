a3a1c143cc7d748c453c541e161abd0bf5f2e310
Reeahn <36976247+Reeahn@users.noreply.github.com>
2018-10-12 11:20:21 +0930

    testing running through the json file

--- Prototype A3/js/recipesAndSearch.js
+++ Prototype A3/js/recipesAndSearch.js
@@ -3,11 +3,10 @@ fetch('../JSON/recipes.json')
     return response.json();
   })
   .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
+    var recipes = JSON.stringify(myJson);
 });
-// var recipes = JSON.parse(recipes)
-// for (i = 0; i < recipes.length; i++) {
-//   document.getElementById('results').innerhtml += recipes[i]
-//   console.log(recipes[i])
-// }
-// console.log("recipes js loaded")
+
+for (i = 0; i < recipes.length; i++) {
+  document.getElementById('results').innerhtml += recipes[i]
+  console.log(recipes[i])
+}
