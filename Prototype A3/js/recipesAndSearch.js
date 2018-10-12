486b59b54e87fb462fbd0e32c0b26969b9685bfc
Reeahn <36976247+Reeahn@users.noreply.github.com>
2018-10-12 11:29:13 +0930

    accessing json is tricky

--- Prototype A3/js/recipesAndSearch.js
+++ Prototype A3/js/recipesAndSearch.js
@@ -4,8 +4,5 @@ fetch('../JSON/recipes.json')
   })
   .then(function(myJson) {
     var recipes = JSON.stringify(myJson);
-    for (i = 0; i < recipes.length; i++) {
-      document.getElementById('results').innerhtml += recipes[i]
-      console.log(recipes[i])
-    }
-});
+    document.getElementById('results').innerhtml = recipes["name"];
+})
