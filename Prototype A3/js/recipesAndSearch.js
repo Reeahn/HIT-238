4ee6a586ae8d368eb8af41d5ce087f66b697c00f
Reeahn <36976247+Reeahn@users.noreply.github.com>
2018-10-12 10:43:30 +0930

    Update Prototype A3/js/recipesAndSearch.js

--- Prototype A3/js/recipesAndSearch.js
+++ Prototype A3/js/recipesAndSearch.js
@@ -2,9 +2,9 @@ fetch('../JSON/recipes.json')
     .then(function(response) {
         console.log(response);
     });
-var recipes = JSON.parse(recipes)
-for (i = 0; i < recipes.length; i++) {
-  document.getElementById('results').innerhtml += recipes[i]
-  console.log(recipes[i])
-}
-console.log("recipes js loaded")
+// var recipes = JSON.parse(recipes)
+// for (i = 0; i < recipes.length; i++) {
+//   document.getElementById('results').innerhtml += recipes[i]
+//   console.log(recipes[i])
+// }
+// console.log("recipes js loaded")
